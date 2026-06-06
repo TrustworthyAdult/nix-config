67ecdbb337004b960e7d35660fa8c9b4f9844f8e
Michael Ross <michaeljrossdev@gmail.com>
2026-06-06 13:19:11 +0100

    use snacks pickers

--- modules/dev/neovim.nix
+++ modules/dev/neovim.nix
@@ -4,6 +4,7 @@
     { pkgs, ... }:
     {
       environment.systemPackages = [
+        pkgs.ripgrep
         (inputs.nvf.lib.neovimConfiguration {
           inherit pkgs;
           modules = [
@@ -22,30 +23,110 @@
                 binds.whichKey.enable = true;
 
                 # --- navigation ---
-                mini.pick.enable = true;
+                utility.snacks-nvim = {
+                  enable = true;
+                  setupOpts.picker.enabled = true;
+                };
                 utility.oil-nvim = {
                   enable = true;
                   gitStatus.enable = true;
                 };
                 maps.normal = {
+                  # files
                   "<leader>ff" = {
-                    action = "<cmd>lua MiniPick.builtin.files()<CR>";
+                    action = "<cmd>lua Snacks.picker.files()<CR>";
                     desc = "Find files";
                     silent = true;
                   };
+                  "<leader>fr" = {
+                    action = "<cmd>lua Snacks.picker.recent()<CR>";
+                    desc = "Recent files";
+                    silent = true;
+                  };
+                  "<leader>f/" = {
+                    action = "<cmd>lua Snacks.picker.lines()<CR>";
+                    desc = "Search buffer lines";
+                    silent = true;
+                  };
+                  # grep
                   "<leader>fg" = {
-                    action = "<cmd>lua MiniPick.builtin.grep_live()<CR>";
+                    action = "<cmd>lua Snacks.picker.grep()<CR>";
                     desc = "Live grep";
                     silent = true;
                   };
+                  # buffers / ui
                   "<leader>fb" = {
-                    action = "<cmd>lua MiniPick.builtin.buffers()<CR>";
+                    action = "<cmd>lua Snacks.picker.buffers()<CR>";
                     desc = "Buffers";
                     silent = true;
                   };
+                  "<leader>fR" = {
+                    action = "<cmd>lua Snacks.picker.resume()<CR>";
+                    desc = "Resume picker";
+                    silent = true;
+                  };
+                  # help / meta
                   "<leader>fh" = {
-                    action = "<cmd>lua MiniPick.builtin.help()<CR>";
+                    action = "<cmd>lua Snacks.picker.help()<CR>";
                     desc = "Help";
+                    silent = true;
+                  };
+                  "<leader>fk" = {
+                    action = "<cmd>lua Snacks.picker.vim_keymaps()<CR>";
+                    desc = "Keymaps";
+                    silent = true;
+                  };
+                  "<leader>fc" = {
+                    action = "<cmd>lua Snacks.picker.vim_history()<CR>";
+                    desc = "Command history";
+                    silent = true;
+                  };
+                  # lsp
+                  "<leader>fs" = {
+                    action = "<cmd>lua Snacks.picker.lsp_symbols()<CR>";
+                    desc = "LSP symbols";
+                    silent = true;
+                  };
+                  "<leader>fd" = {
+                    action = "<cmd>lua Snacks.picker.diagnostics()<CR>";
+                    desc = "Diagnostics";
+                    silent = true;
+                  };
+                  # git
+                  "<leader>fG" = {
+                    action = "<cmd>lua Snacks.picker.git_files()<CR>";
+                    desc = "Git files";
+                    silent = true;
+                  };
+                  "<leader>ft" = {
+                    action = "<cmd>lua Snacks.picker.git_status()<CR>";
+                    desc = "Git status";
+                    silent = true;
+                  };
+                  "<leader>fl" = {
+                    action = "<cmd>lua Snacks.picker.git_log()<CR>";
+                    desc = "Git log";
+                    silent = true;
+                  };
+                  # vim state
+                  "<leader>fm" = {
+                    action = "<cmd>lua Snacks.picker.vim_marks()<CR>";
+                    desc = "Marks";
+                    silent = true;
+                  };
+                  "<leader>fj" = {
+                    action = "<cmd>lua Snacks.picker.vim_jumps()<CR>";
+                    desc = "Jump list";
+                    silent = true;
+                  };
+                  "<leader>fu" = {
+                    action = "<cmd>lua Snacks.picker.undo()<CR>";
+                    desc = "Undo history";
+                    silent = true;
+                  };
+                  "<leader>fq" = {
+                    action = "<cmd>lua Snacks.picker.qf()<CR>";
+                    desc = "Quickfix list";
                     silent = true;
                   };
                 };
