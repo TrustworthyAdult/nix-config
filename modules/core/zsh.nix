5420215ec387bf210e424fa3ebc5407f7b432d00
Michael Ross <michaeljrossdev@gmail.com>
2026-06-07 22:53:57 +0100

    quality of life zsh stuff

--- modules/core/zsh.nix
+++ modules/core/zsh.nix
@@ -6,6 +6,8 @@
       programs.zsh = {
         enable = true;
         syntaxHighlighting.enable = true;
+        autosuggestions.enable = true;
+        enableLsColors = true;
       };
       programs.starship.enable = true;
       environment.systemPackages = [ pkgs.zoxide ];
